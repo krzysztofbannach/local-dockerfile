b2fe60c95a32a123e302aa2c6a6031cdd6118d50
krzysztof.bannach <krzysztof.bannach@dynatrace.com>
2026-07-08 14:36:11 +0200

    update dok-3rd-party image tag

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DOK_3RD_PARTY_IMAGE=dok-3rd-party:0.0.20-20260219-113948
+export DOK_3RD_PARTY_IMAGE=dok-3rd-party:0.0.31-20260618-113459
 docker pull 256120352618.dkr.ecr.us-east-1.amazonaws.com/dok-cicd-registry/$DOK_3RD_PARTY_IMAGE
 
 DOCKER_BUILDKIT=1 docker build -t local-docker:latest -f Dockerfile --build-arg BUILDKIT_INLINE_CACHE=1 --build-arg OPERATORS_DOK_3RD_PARTY_IMAGE=$DOK_3RD_PARTY_IMAGE --secret id=github_token,env=GITHUB_TOKEN .
